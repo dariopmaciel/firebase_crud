23143832e5af2a1fa64587d186d6ff37e36c5d8d
Dario de Paula MAciel <dariodepaulamaciel@hotmail.com>
2023-02-28 16:49:13 -0300

    first commit

---
+++ lib/checagem_page.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class ChecagemPage extends StatefulWidget {
+  const ChecagemPage({super.key});
+
+  @override
+  State<ChecagemPage> createState() => _ChecagemPageState();
+}
+
+class _ChecagemPageState extends State<ChecagemPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CHECAGEM PAGE'),
+      ),
+      body: Center(child: CircularProgressIndicator()),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crud/checagem_page.dart';
+import 'package:firebase_crud/firebase_options.dart';
+import 'package:firebase_crud/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: true,
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
+      ),
+      home: const ChecagemPage(),
+    );
+  }
+}
