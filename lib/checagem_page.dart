6de4520f40efe50b066c3dca242ef873556ac280
Dario de Paula MAciel <dariodepaulamaciel@hotmail.com>
2023-02-28 16:53:32 -0300

    atz

--- lib/checagem_page.dart
+++ lib/checagem_page.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_crud/home_page.dart';
+import 'package:firebase_crud/login_page.dart';
 import 'package:flutter/material.dart';
 
 class ChecagemPage extends StatefulWidget {
@@ -15,9 +17,21 @@ class _ChecagemPageState extends State<ChecagemPage> {
 
     FirebaseAuth.instance.authStateChanges().listen((User? user) {
       if (user == null) {
-        print('User is currently signed out!');
+        print('Você não tem usuário Logado!');
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => LoginPage(),
+          ),
+        );
       } else {
-        print('User is signed in!');
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomePage(),
+          ),
+        );
+        print('Usuário Logado!');
       }
     });
   }
